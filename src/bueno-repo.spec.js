d8d21c9da9483eaa1cfa0ec05ccd15a611d4e490
Benjamin Vellacott <bmvellacott@gmail.com>
2020-04-18 02:20:33 +0300

    switch to tap

--- src/bueno-repo.spec.js
+++ src/bueno-repo.spec.js
@@ -1,12 +1,7 @@
 var buenoRepo = require('.')
 buenoRepo.setupAliases()
 
-var assert = require('assert');
-
+const tap = require('tap')
 var aMonoRepoPackage = require('a-mono-repo-package');
 
-describe('bueno-repo', function () {
-  it('should resolve a-mono-repo-package and print Hello World!', function() {
-    assert.equal(aMonoRepoPackage.helloWorld(), 'Hello World!');
-  });
-});
+tap.equal(aMonoRepoPackage.helloWorld(), 'Hello World!');
