56533f7cf3662e752abdafab021e33c707280515
Benjamin Vellacott <bmvellacott@gmail.com>
2020-04-20 13:20:05 +0300

    fix test script

--- src/bueno-repo.spec.js
+++ src/bueno-repo.spec.js
@@ -1,12 +1,26 @@
-// snippet to setup the aliasing
-require('.').setupAliases()
-
 var assert = require('assert')
 
-// require the aliased packages
-var aMonoRepoPackage = require('@my-libs/a-mono-repo-test-package');
-var anotherMonoRepoPackage = require('@my-libs/another-mono-repo-test-package');
+function testGreen(message) {
+  console.log('\x1b[32m%s\x1b[0m', message)
+}
 
-// verify that the packages have been resolved correctly
-assert.equal(aMonoRepoPackage.helloWorld(), 'Hello World!');
-assert.equal(anotherMonoRepoPackage.helloWorld(), 'Hello Another World!');
+function testRed(message) {
+  console.log('\x1b[31m%s\x1b[0m', message)
+}
+
+try {
+  // snippet to setup the aliasing
+  require('.').setupAliases()
+
+  // require the aliased packages
+  var aMonoRepoPackage = require('@my-libs/a-mono-repo-test-package');
+  var anotherMonoRepoPackage = require('@my-libs/another-mono-repo-test-package');
+
+  // verify that the packages have been resolved correctly
+  assert.equal(aMonoRepoPackage.helloWorld(), 'Hello World!');
+  assert.equal(anotherMonoRepoPackage.helloWorld(), 'Hello Another World!');
+  testGreen('resolved the aliased packages without error')
+} catch (e) {
+  testRed(e.message)
+  throw e
+}
