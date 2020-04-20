65b7eadfbf8eb54225a44258973e84f0bbb87b9f
Benjamin Vellacott <bmvellacott@gmail.com>
2020-04-20 12:44:08 +0300

    switch from tap to tweenytest

--- src/bueno-repo.spec.js
+++ src/bueno-repo.spec.js
@@ -1,8 +1,20 @@
-require('.').setupAliases()
 
-const tap = require('tap')
-var aMonoRepoPackage = require('@my-libs/a-mono-repo-test-package');
-var anotherMonoRepoPackage = require('@my-libs/another-mono-repo-test-package');
 
-tap.equal(aMonoRepoPackage.helloWorld(), 'Hello World!');
-tap.equal(anotherMonoRepoPackage.helloWorld(), 'Hello Another World!');
+var assert = require('assert')
+
+module.exports = {
+  beforeAll: function() {
+    // snippet to setup the aliasing
+    require('.').setupAliases()
+  },
+
+  resolvesTheAliases: function() {
+    // require the aliased packages
+    var aMonoRepoPackage = require('@my-libs/a-mono-repo-test-package');
+    var anotherMonoRepoPackage = require('@my-libs/another-mono-repo-test-package');
+    
+    // verify that the packages have been resolved correctly
+    assert.equal(aMonoRepoPackage.helloWorld(), 'Hello World!');
+    assert.equal(anotherMonoRepoPackage.helloWorld(), 'Hello Another World!');
+  },
+}
